d1314357a20ef38ee126ce1127e4358874ad5441
ThaTechMaestro <muse.pearls@gmail.com>
2025-02-07 02:16:59 +0000

    array: merge sorted array

---
+++ Array/merge_sorted_array/soln.py
@@ -0,0 +1,11 @@
+'''
+Question
+    https://leetcode.com/problems/merge-sorted-array/description/
+
+Pattern: Use of 3 Pointers
+
+Oneliner hint:
+    Work backward with the two arrays,
+    one pointer at the end of both & one at the last value for nums1
+    
+'''
